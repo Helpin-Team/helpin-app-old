eb955b9416adf5096e9d9c399317a9037697f932
Efraim Andrade <efraim.morais@fatecitapetininga.edu.br>
2019-04-20 23:01:31 -0300

    teste

--- src/config/Reactotron.js
+++ src/config/Reactotron.js
@@ -4,8 +4,8 @@ import sagaPlugin from 'reactotron-redux-saga';
 
 if (__DEV__) {
   const tron = Reactotron
-    // .configure({ host: '192.168.15.13' }) // casa
-    .configure({ host: '192.168.1.27' }) // iClouds
+    .configure({ host: '192.168.15.18' }) // casa
+    // .configure({ host: '192.168.1.27' }) // iClouds
     // .configure({ host: '192.168.43.219' }) // celular
     .useReactNative()
     // .use(reactotronRedux())
